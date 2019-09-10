e8870688bf6b96e0fcd8abbcbb6e5043d1a0824f
darrylgit <darrylsmithup@gmail.com>
2019-09-09 21:11:12 -0400

    Install SASS

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,15 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
