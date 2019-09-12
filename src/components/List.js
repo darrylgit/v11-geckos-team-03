f07dcb0a70eabc9a43e1ddc9f0ee8b8ec8bb2ea8
darrylgit <darrylsmithup@gmail.com>
2019-09-12 18:56:19 -0400

    Markup add new card button

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,41 @@
 import React from "react";
 
-const List = props => {
-  const removeList = () => {
-    props.remove(props.listTitle);
+class List extends React.Component {
+  state = {
+    backgroundColor: "#c5c9eb"
+  };
+  removeList = () => {
+    this.props.remove(this.props.listTitle);
   };
 
-  return (
-    <div className="list">
-      <div className="list__remove" onClick={removeList}>
-        &times;
+  darkenBackground = () => {
+    this.setState({ backgroundColor: "#9093ad" });
+  };
+
+  lightenBackground = () => {
+    this.setState({ backgroundColor: "#c5c9eb" });
+  };
+
+  render() {
+    return (
+      <div className="list">
+        <div className="list__remove" onClick={this.removeList}>
+          &times;
+        </div>
+        <h2 className="list__heading">{this.props.listTitle}</h2>
+        <div className="list__cards"></div>
+        <div
+          className="list__add-card"
+          style={{ backgroundColor: this.state.backgroundColor }}
+          onMouseEnter={this.darkenBackground}
+          onMouseLeave={this.lightenBackground}
+        >
+          <span className="list__add-card--plus">+</span>
+          <span className="list__add-card--label">Add new card</span>
+        </div>
       </div>
-      <h2 className="list__heading">{props.listTitle}</h2>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default List;
