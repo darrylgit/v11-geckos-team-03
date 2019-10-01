ef031ad9c2f651e037fe2be0e7b91b1876296495
darrylgit <darrylsmithup@gmail.com>
2019-10-01 19:35:50 -0400

    Make cards draggable

--- src/components/board/list/Card.js
+++ src/components/board/list/Card.js
@@ -1,9 +1,37 @@
 import React from "react";
+import { ItemTypes } from "../Constants";
+import { useDrag } from "react-dnd";
 
+/*
 class Card extends React.Component {
+  
+
   render() {
-    return <div className="card">{this.props.cardTitle}</div>;
+    
   }
+}
+*/
+
+function Card(props) {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.CARD },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  });
+
+  return (
+    <div
+      ref={drag}
+      className="card"
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        cursor: "move"
+      }}
+    >
+      {props.cardTitle}
+    </div>
+  );
 }
 
 export default Card;
