7fb6b0fc3d98b78db8f30fd276d232b6a98a13b8
darrylgit <darrylsmithup@gmail.com>
2019-10-30 18:38:27 -0400

    Updates

--- precache-manifest.3c93cf74b7b65e4bf550f0f9922b2a92.js
+++ precache-manifest.3c93cf74b7b65e4bf550f0f9922b2a92.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6843ed95233129c608a3d8287ff7546",
+    "revision": "a9474befa6bc521de475c3d2c3b335ff",
     "url": "/v11-geckos-team-03/index.html"
   },
   {
-    "revision": "c5ba3d3c5d0b243a2f83",
-    "url": "/v11-geckos-team-03/static/css/main.4ed0bf42.chunk.css"
+    "revision": "5c777d0e093ce87dbb02",
+    "url": "/v11-geckos-team-03/static/css/main.3db90539.chunk.css"
   },
   {
     "revision": "a61f3187a2da6631b4fe",
     "url": "/v11-geckos-team-03/static/js/2.efbe6e83.chunk.js"
   },
   {
-    "revision": "c5ba3d3c5d0b243a2f83",
+    "revision": "5c777d0e093ce87dbb02",
     "url": "/v11-geckos-team-03/static/js/main.c4f00dad.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/v11-geckos-team-03/precache-manifest.11d6ef371279852152e263556a3cf33b.js"
+  "/v11-geckos-team-03/precache-manifest.3c93cf74b7b65e4bf550f0f9922b2a92.js"
 );
 
 self.addEventListener('message', (event) => {
